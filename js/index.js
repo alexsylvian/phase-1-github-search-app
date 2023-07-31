a7662c9d1edc91d074e7f97f37763fbb3e45e652
alexsylvian <lazeratus1@gmail.com>
2023-07-31 17:55:36 -0400

    complete, needs more practice

--- js/index.js
+++ js/index.js
@@ -1,159 +1,106 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
     const searchForm = document.getElementById('github-form');
     const searchInput = document.getElementById('search');
     const userList = document.getElementById('user-list');
     const reposList = document.getElementById('repos-list');
-
-    searchForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        searchContent = searchInput.value
-        console.log(searchContent)
-        if (searchContent === '') {
-            return;
-        }
-        searchInput.value = ''
-
-        searchUsers(searchContent)
-            .then (users => displayUsers(users))
-
-        function searchUsers(query) {
-            const apiUrl = `https://api.github.com/search/users?q=${query}`
-            return fetch(apiUrl)
-        }
-
-        function displayUsers(users)
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const searchForm = document.getElementById('github-form');
-//     const searchInput = document.getElementById('search');
-//     const userList = document.getElementById('user-list');
-//     const reposList = document.getElementById('repos-list');
   
-//     searchForm.addEventListener('submit', function (e) {
-//       e.preventDefault();
-//       const searchTerm = searchContent.trim();
-//       if (searchTerm === '') {
-//         return;
-//       }
+    searchForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const searchTerm = searchContent.trim();
+      if (searchTerm === '') {
+        return;
+      }
   
-//       searchUsers(searchTerm)
-//         .then(function (users) {
-//           displayUsers(users);
-//         })
-//         .catch(function (error) {
-//           console.error('Error searching users:', error);
-//         });
-//     });
+      searchUsers(searchTerm)
+        .then(function (users) {
+          displayUsers(users);
+        })
+        .catch(function (error) {
+          console.error('Error searching users:', error);
+        });
+    });
   
-//     function searchUsers(query) {
-//       const apiUrl = `https://api.github.com/search/users?q=${query}`;
-//       return fetch(apiUrl)
-//         .then(function (response) {
-//           if (!response.ok) {
-//             throw new Error('Network response was not ok');
-//           }
-//           return response.json();
-//         })
-//         .then(function (data) {
-//           return data.items;
-//         })
-//         .catch(function (error) {
-//           console.error('Error searching users:', error);
-//           throw error;
-//         });
-//     }
+    function searchUsers(query) {
+      const apiUrl = `https://api.github.com/search/users?q=${query}`;
+      return fetch(apiUrl)
+        .then(function (response) {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(function (data) {
+          return data.items;
+        })
+        .catch(function (error) {
+          console.error('Error searching users:', error);
+          throw error;
+        });
+    }
   
-//     function displayUsers(users) {
-//       userList.innerHTML = '';
-//       reposList.innerHTML = '';
+    function displayUsers(users) {
+      userList.innerHTML = '';
+      reposList.innerHTML = '';
   
-//       users.forEach(function (user) {
-//         const userCard = createUserCard(user);
-//         userList.appendChild(userCard);
+      users.forEach(function (user) {
+        const userCard = createUserCard(user);
+        userList.appendChild(userCard);
   
-//         userCard.addEventListener('click', function () {
-//           getUserRepos(user.login)
-//             .then(function (repos) {
-//               displayRepos(repos);
-//             })
-//             .catch(function (error) {
-//               console.error('Error getting user repositories:', error);
-//             });
-//         });
-//       });
-//     }
+        userCard.addEventListener('click', function () {
+          getUserRepos(user.login)
+            .then(function (repos) {
+              displayRepos(repos);
+            })
+            .catch(function (error) {
+              console.error('Error getting user repositories:', error);
+            });
+        });
+      });
+    }
   
-//     function getUserRepos(username) {
-//       const apiUrl = `https://api.github.com/users/${username}/repos`;
-//       return fetch(apiUrl)
-//         .then(function (response) {
-//           if (!response.ok) {
-//             throw new Error('Network response was not ok');
-//           }
-//           return response.json();
-//         })
-//         .catch(function (error) {
-//           console.error('Error getting user repositories:', error);
-//           throw error;
-//         });
-//     }
+    function getUserRepos(username) {
+      const apiUrl = `https://api.github.com/users/${username}/repos`;
+      return fetch(apiUrl)
+        .then(function (response) {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .catch(function (error) {
+          console.error('Error getting user repositories:', error);
+          throw error;
+        });
+    }
   
-//     function displayRepos(repos) {
-//       reposList.innerHTML = '';
+    function displayRepos(repos) {
+      reposList.innerHTML = '';
   
-//       repos.forEach(function (repo) {
-//         const repoItem = document.createElement('li');
-//         const repoLink = document.createElement('a');
-//         repoLink.href = repo.html_url;
-//         repoLink.textContent = repo.name;
-//         repoItem.appendChild(repoLink);
-//         reposList.appendChild(repoItem);
-//       });
-//     }
+      repos.forEach(function (repo) {
+        const repoItem = document.createElement('li');
+        const repoLink = document.createElement('a');
+        repoLink.href = repo.html_url;
+        repoLink.textContent = repo.name;
+        repoItem.appendChild(repoLink);
+        reposList.appendChild(repoItem);
+      });
+    }
   
-//     function createUserCard(user) {
-//       const userCard = document.createElement('li');
-//       userCard.classList.add('user-card');
+    function createUserCard(user) {
+      const userCard = document.createElement('li');
+      userCard.classList.add('user-card');
   
-//       const avatar = document.createElement('img');
-//       avatar.src = user.avatar_url;
-//       avatar.alt = `${user.login}'s avatar`;
-//       userCard.appendChild(avatar);
+      const avatar = document.createElement('img');
+      avatar.src = user.avatar_url;
+      avatar.alt = `${user.login}'s avatar`;
+      userCard.appendChild(avatar);
   
-//       const usernameLink = document.createElement('a');
-//       usernameLink.href = user.html_url;
-//       usernameLink.textContent = user.login;
-//       userCard.appendChild(usernameLink);
+      const usernameLink = document.createElement('a');
+      usernameLink.href = user.html_url;
+      usernameLink.textContent = user.login;
+      userCard.appendChild(usernameLink);
   
-//       return userCard;
-//     }
-//   });
+      return userCard;
+    }
+  });
   
